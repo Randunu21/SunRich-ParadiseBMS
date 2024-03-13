d18fbaf93fdfff0b236006f2a6ab12facfcd2078
Randunu21 <randunudissanayake21@gmail.com>
2024-03-14 01:15:22 +0530

    routes and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 require("dotenv").config();
+const orderRoutes = require("./routes/orders");
 
 const mongoose = require("mongoose");
 
 const app = express();
 
+//middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+
+//routes
+app.use("/api/orders", orderRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
