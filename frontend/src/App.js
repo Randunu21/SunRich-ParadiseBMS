13cf0daf730601f7be1b79769ccd0e58ed052c2d
TharindaDinethri <tharindadinethri2000@gmail.com>
2024-04-20 02:11:12 +0530

    push 2nd time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter,Routes,Route } from 'react-router-dom'; 
+import Home from './Pages/ProductManagement/Home';
+import HomeCategory from './Pages/ProductManagement/HomeCategory';
+import LoginSignup from './Pages/ProductManagement/LoginSignup';
 
 function App() {
   return (
   <div>
+    <BrowserRouter>
     <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/coconutrelated' element={<HomeCategory category="coconutrelated"/>}/>
+      <Route path='/spices' element={<HomeCategory category="spices"/>}/>
+      <Route path="product" element={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+    </Routes>
+    </BrowserRouter>
+    
   </div>
   );
 }
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Navbar.css'
-export const Navbar = () => {
+
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assert/cart_icon.png'
+
+const Navbar = () => {
+
+  const [menu,setMenu] = useState("home")
+
+
   return (
     <div className='navbar'>
-        
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>Sun Rich Paradise</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={()=>{setMenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("coconutrelated")}}>Coconut Related{menu==="coconutrelated"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("spices")}}>Spices{menu==="spices"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("aboutus")}}>About Us{menu==="aboutus"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("contactus")}}>Contact Us{menu==="contactus"?<hr/>:<></>}</li>
+      </ul>
+      <div className="nav-login-cart">
+         <button>Login</button>
+         <img src={cart_icon} alt="" />
+         <div className="nav-cart-count">0</div>
+      </div>
     </div>
   )
 }
