6a5e286783c64c73c30eac36b5c141308e54a15a
Ridmi2002 <rdimi.pomodya426@gmail.com>
2024-05-15 17:08:16 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,34 @@
-const express = require("express");
-require("dotenv").config();
+require('dotenv').config()
+const express = require("express")
+const mongoose = require("mongoose")
+const supplierRoutes = require('./routes/supplierRoutes')
 
-const mongoose = require("mongoose");
+const cors = require("cors")
 
 const app = express();
+app.use(cors())
+
+//global middleware
+app.use(express.json())
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+    console.log(req.path, req.method)
+    next()
+})
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
+//routes
+
+app.use('/api/supplier', supplierRoutes)
+
+
+//connect to db
+
+mongoose.connect(process.env.MONGO_URI)
+ .then(() => {
     app.listen(4000, () => {
-      console.log("Listening to port 4000");
-    });
-  })
-  .catch((error) => {
+        console.log("Listening to port 4000");
+    })
+ })
+ .catch((error) => {
     console.log(error);
-  });
+ })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import SupplierForm from './pages/suppliers/suppliers';
+import OrderComponent from './pages/suppliers/orderPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/suppliers/home' element={<SupplierForm/>}></Route>
+          <Route path='/suppliers/order' element={<OrderComponent/>}></Route>
+       </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
