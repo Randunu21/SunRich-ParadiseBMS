30b7842dc733a521a6c10035023181d2111a6e6e
SachinthaRajapaksha <sachintharajapakshaofficial@gmail.com>
2024-05-16 05:52:59 +0530

    sad

--- frontend/src/pages/adminPage.js
+++ frontend/src/pages/adminPage.js
@@ -110,7 +110,7 @@ const AdminDashboard = () => {
           >
             <div className="card-body">
               <h2 style={{ marginTop: '15px' }}>Financial Management</h2>
-              <a href="/financial-management" className="hidden-link" style={hiddenLinkStyle}>
+              <a href="/Financial/dash" className="hidden-link" style={hiddenLinkStyle}>
                 Go to Financial Management System
               </a>
             </div>
